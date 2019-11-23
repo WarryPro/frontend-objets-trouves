38f6fddaa2fbfc2b05c7afdb74389ae85396c29a
dany <dannyfr.03@gmail.com>
2019-11-23 02:03:41 +0100

    Feat: created component Header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//ROUTES 
+import LastItems from './components/LastItemsComponent';
 
 Vue.config.productionTip = false
 
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
+
+
+const routes = [
+  { path: '/', 
+    component: LastItems },
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
