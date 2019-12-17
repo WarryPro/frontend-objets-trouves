89bd06f0d321ee4afff19914a73e7bc5f0cfab5b
dany <dannyfr.03@gmail.com>
2019-12-17 20:03:12 +0100

    pro

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const cors = require('cors');
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
-app.use(cors())
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
